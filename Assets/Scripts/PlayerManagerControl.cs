8a4636e56e63d81225085208f089f1715767d366
Stephen Mills <StephenMills@letu.edu>
2019-05-03 00:57:21 -0500

    changed comma

--- Assets/Scripts/PlayerManagerControl.cs
+++ Assets/Scripts/PlayerManagerControl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManagerControl : MonoBehaviourPun : IPunInstantiateMagicCallback
+public class PlayerManagerControl : MonoBehaviourPun , IPunInstantiateMagicCallback
 {
     public void EnableComponents()
     {
