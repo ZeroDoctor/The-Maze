c471250e3d50ddd6bde0e33978c6ff610c27d6ac
Stephen Mills <StephenMills@letu.edu>
2019-05-05 22:41:40 -0500

    Added pause menu and logic
    Added pause menu, and control script

---
+++ Assets/Scripts/PlayerPause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPause : MonoBehaviour
+{
+    [Header("Pause Menu")]
+    public GameObject pauseMenu;
+
+    [Header("Menu Buttons")]
+    public Button exit;
+    public Button resume;
+
+    [Header("Pause Key")]
+    public KeyCode pause = KeyCode.Escape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pause) && pauseMenu.active == false)
+        {
+            Debug.Log("Pause going active");
+            pauseMenu.SetActive(true);
+        }
+        
+        if (Input.GetKeyDown(pause) && pauseMenu.active == true)
+        {
+            Debug.Log("Pause window open, closing");
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    //On exit button click, leave game and return to main menu
+    public void OnExitClick()
+    {
+        Debug.Log("Exit Clicked");
+    }
+
+    //On resume button click, close menu, lock cursor, and make cursor invisible
+    public void OnResumeClick()
+    {
+        Debug.Log("Resume Clicked");
+    }
+}
