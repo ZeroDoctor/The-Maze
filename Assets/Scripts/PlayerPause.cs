0657f7c73b3f1fc733d7b2070105b5fc0d88aac4
Stephen Mills <StephenMills@letu.edu>
2019-05-06 22:22:15 -0500

    Updates
    Fixed pause menu, made game manager a prefab

--- Assets/Scripts/PlayerPause.cs
+++ Assets/Scripts/PlayerPause.cs
@@ -24,7 +24,7 @@ public class PlayerPause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(pause) && !pauseMenu.activeSelf)
+        if (Input.GetKeyDown(pause))
         {
             if (pauseMenu.activeInHierarchy == true)
             {
