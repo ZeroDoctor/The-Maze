2680e32e697f0324e98bfc940cf53e5b624d6dcb
Zerulus <danielrcastro10@gmail.com>
2019-05-07 00:20:57 -0500

    updated game manager for maze

--- Assets/Scripts/Network/GameManager.cs
+++ Assets/Scripts/Network/GameManager.cs
@@ -2,6 +2,7 @@
 using Photon.Realtime;
 using System;
 using Random = System.Random;
+using UnityRandom = UnityEngine.Random;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,12 +47,42 @@ public class GameManager : MonoBehaviourPunCallbacks
         Player playerName = PhotonNetwork.LocalPlayer;
         int index = Array.IndexOf(players, playerName);
 
-        int x = getrandom.Next(50, 400);
-        float y = 0.8f;
-        int z = getrandom.Next(50, 400);
+        Vector3 randPos = Vector3.zero;
+        int myCheck = 0;
+        do
+        {
+            myCheck = 0;
+            randPos = new Vector3(UnityRandom.Range(50f, 400f), 6f, UnityRandom.Range(50f, 400f));
+            Collider[] hitColliders = Physics.OverlapSphere(randPos, 5f);
+            for (int j = 0; j < hitColliders.Length; j++)
+            {
+                if (hitColliders[j].tag == "Outside")
+                {
+                    myCheck++;
+                }
+            }
+
+            // verify that position is above terrain
+            RaycastHit hit;
+            if (Physics.Raycast(randPos, Vector3.up, out hit))
+            {
+                float distanceToGround = hit.distance;
+                if (hit.transform.name == "Terrain")
+                {
+                    // there is something else above us
+                    myCheck = 1;
+                }
+                else
+                {
+                    // all is good
+                    randPos.y -= (distanceToGround - 0.8f);
+                }
+            }
+        } while (myCheck > 0);
+        Vector3 location = randPos;
 
         spawn.spawnPoint = new GameObject("Spawn");
-        spawn.spawnPoint.transform.position = new Vector3((float)x, y, (float)z);
+        spawn.spawnPoint.transform.position = location;
         Debug.Log("Instantiating LocalPlayer: " + playerName.NickName + " at spawn point " + index);
         PhotonNetwork.Instantiate(templarPrefab.name, spawn.spawnPoint.transform.position, spawn.spawnPoint.transform.rotation, 0);
     }
